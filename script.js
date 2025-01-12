4a28879f61f1f217e12da127dc56622fe0f5e0e6
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-13 02:13:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function submitForm(){
+	let a=document.getElementById("First Name").value;
+	let b=document.getElementById("Last Name").value;
+	let c=document.getElementById("Phone Number").value;
+	let d=document.getElementById("Email ID").value;
+
+	let message = "First Name: " + a + "\nLast Name: " + b + "\nPhone Number: " + c + "\nEmail ID: " + d;
+	alert(message);
+}
